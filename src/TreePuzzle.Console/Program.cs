dadff1f42dea8c22923f5b5dde3f8c46e4a240dc
Robert Segal <robertsegal@Roberts-MacBook-Pro-14.local>
2025-04-30 21:17:39 -0600

    changed prompts

--- src/TreePuzzle.Console/Program.cs
+++ src/TreePuzzle.Console/Program.cs
@@ -5,18 +5,28 @@ public class CLI
     public static void Main()
     {
         string original = "(id, name, email, type(id, name, customFields(c1, c2, c3)), externalId)";
+        string valueToParse = original;
         
-        Console.WriteLine($"Parsing {original}");
+        Console.WriteLine($"Original is {original}. Do you wish to parse this (y/n)?");
+        bool use_original = Convert.ToString(Console.ReadLine()) == "y" ? true : false;
+        
+        if (!use_original)
+        {
+            Console.WriteLine("What would you like to parse?");
+            valueToParse = Convert.ToString(Console.ReadLine());
+        }
+        
+        Console.WriteLine($"Parsing {valueToParse}");
 
         int startIndex = 0;
         
-        var tree = Tree.Parse(original, ref startIndex);
+        var tree = Tree.Parse(valueToParse, ref startIndex);
 
-        Console.WriteLine("\nUnsorted original translation is: ");
+        Console.WriteLine("\nUnsorted translation is: ");
         
         Tree.Print(tree, 0);
         
-        Console.WriteLine("\nSorted original translation is: ");
+        Console.WriteLine("\nSorted translation is: ");
 
         var sortedTree = Tree.Sort(tree);
         
