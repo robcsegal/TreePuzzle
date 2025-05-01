fb0f125f88e35dee93b7c4117771b18f940156f5
Robert Segal <robertsegal@Roberts-MacBook-Pro-14.local>
2025-05-01 06:10:11 -0600

    added do you wish to continue and a constant

--- src/TreePuzzle.Console/Constants.cs
+++ src/TreePuzzle.Console/Constants.cs
@@ -3,4 +3,5 @@ namespace TreePuzzle.Console;
 public static class Constants
 {
     public const int NUM_OF_SPACES_FOR_INDENT = 2;
+    public const string DEFAULT_INPUT = "(id, name, email, type(id, name, customFields(c1, c2, c3)), externalId)";
 }
--- src/TreePuzzle.Console/Program.cs
+++ src/TreePuzzle.Console/Program.cs
@@ -4,32 +4,42 @@ public class CLI
 {
     public static void Main()
     {
-        string original = "(id, name, email, type(id, name, customFields(c1, c2, c3)), externalId)";
+        string original = Constants.DEFAULT_INPUT;
         string valueToParse = original;
-        
-        Console.WriteLine($"Original is {original}. Do you wish to parse this (y/n)?");
-        bool use_original = Convert.ToString(Console.ReadLine()) == "y" ? true : false;
-        
-        if (!use_original)
+        bool isContinued = true;
+        bool useOriginal = true;
+
+        while (isContinued)
         {
-            Console.WriteLine("What would you like to parse?");
-            valueToParse = Convert.ToString(Console.ReadLine());
+        
+            Console.WriteLine($"Original is {original}. Do you wish to parse this (y/n)?");
+            useOriginal = Convert.ToString(Console.ReadLine()) == "y" ? true : false;
+            
+            if (!useOriginal)
+            {
+                Console.WriteLine("What would you like to parse?");
+                valueToParse = Convert.ToString(Console.ReadLine());
+            }
+        
+            Console.WriteLine($"Parsing {valueToParse}");
+
+            int startIndex = 0;
+        
+            var tree = Tree.Parse(valueToParse, ref startIndex);
+
+            Console.WriteLine("\nUnsorted translation is: ");
+        
+            Tree.Print(tree, 0);
+        
+            Console.WriteLine("\nSorted translation is: ");
+
+            var sortedTree = Tree.Sort(tree);
+        
+            Tree.Print(sortedTree, 0);
+            
+            Console.WriteLine("Do you want to process another set (y/n)?");
+            isContinued = Convert.ToString(Console.ReadLine()) == "y" ? true : false;
         }
         
-        Console.WriteLine($"Parsing {valueToParse}");
-
-        int startIndex = 0;
-        
-        var tree = Tree.Parse(valueToParse, ref startIndex);
-
-        Console.WriteLine("\nUnsorted translation is: ");
-        
-        Tree.Print(tree, 0);
-        
-        Console.WriteLine("\nSorted translation is: ");
-
-        var sortedTree = Tree.Sort(tree);
-        
-        Tree.Print(sortedTree, 0);
     }
 }
